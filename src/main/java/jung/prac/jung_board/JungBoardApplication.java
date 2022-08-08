e5c6574e1eb3fe795096be87df1bd859f53a3b48
ap3334 <chwil2989@naver.com>
2022-08-08 10:46:57 +0900

    hello world 출력 컨트롤러 생성

---
+++ src/main/java/jung/prac/jung_board/JungBoardApplication.java
@@ -0,0 +1,13 @@
+package jung.prac.jung_board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JungBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JungBoardApplication.class, args);
+    }
+
+}
