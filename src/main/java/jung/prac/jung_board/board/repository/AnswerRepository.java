0372929c9c7dd70f4c44e745d1b518e42b9897d0
ap3334 <chwil2989@naver.com>
2022-08-08 11:47:23 +0900

    메서드 추가 및 수정

--- src/main/java/jung/prac/jung_board/board/repository/AnswerRepository.java
+++ src/main/java/jung/prac/jung_board/board/repository/AnswerRepository.java
@@ -1,9 +1,10 @@
 package jung.prac.jung_board.board.repository;
 
+import jung.prac.jung_board.board.entity.Answer;
 import jung.prac.jung_board.board.entity.Question;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface AnswerRepository extends JpaRepository<Question, Long> {
+public interface AnswerRepository extends JpaRepository<Answer, Long> {
 }
--- src/main/java/jung/prac/jung_board/board/repository/QuestionRepository.java
+++ src/main/java/jung/prac/jung_board/board/repository/QuestionRepository.java
@@ -4,6 +4,15 @@ import jung.prac.jung_board.board.entity.Question;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface QuestionRepository extends JpaRepository<Question, Long> {
+
+    Question findBySubject(String subject);
+
+    Question findBySubjectAndContent(String subject, String content);
+
+    List<Question> findBySubjectLike(String subject);
 }
