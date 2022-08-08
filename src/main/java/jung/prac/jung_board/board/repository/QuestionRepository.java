0d27319d3e7d6dc64d88f40d8d9717b6714f55af
ap3334 <chwil2989@naver.com>
2022-08-08 11:28:30 +0900

    repository 생성

---
+++ src/main/java/jung/prac/jung_board/board/repository/QuestionRepository.java
@@ -0,0 +1,9 @@
+package jung.prac.jung_board.board.repository;
+
+import jung.prac.jung_board.board.entity.Question;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface QuestionRepository extends JpaRepository<Question, Long> {
+}
